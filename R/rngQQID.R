2b32c0eb44e1ac9d06a2a83ec1343496051bdf09
hyginn <boris.steipe@utoronto.ca>
2019-04-18 01:12:59 -0400

    reconceptualized, now restores global RNG state as per Martin Maechler's advice, added tests

--- R/rngQQID.R
+++ R/rngQQID.R
@@ -1,69 +1,162 @@
 # rngQQID.R
 
-#' rngQQID.
+#'rngQQID.
 #'
-#' \code{rngQQID} uses R's random number generator to generate a vector of
-#' pseudorandom QQIDs.
+#'\code{rngQQID} uses R's random number generator to generate a vector of
+#'pseudorandom QQIDs.
 #'
-#' The function \code{rngQQID} generates a vector of \code{n} pseudo-random
-#' QQIDs using R's inbuilt random number generator. Internally, the QQIDs are
-#' constructed from randomly sampled individual bits, thus the resulting QQIDs
-#' do not suffer from distributional issues that arise from mapping large
-#' floating point numbers to a continuous range of integers.
+#'The function \code{rngQQID} generates a vector of \code{n} pseudo-random QQIDs
+#'using R's inbuilt random number generator. Internally, the QQIDs are
+#'constructed from randomly sampled individual bits, thus the resulting QQIDs do
+#'not suffer from distributional issues that arise from mapping large floating
+#'point numbers to a continuous range of integers.
 #'
+#'@section qQQIds vs. rngQQIDs: In theory the chance of a key collision between
+#'  the pseudo-random QQIDs produced by \code{rngQQID()} is small if the RNG is
+#'  initialized with a 64 bit number: a 50%% probability of at least one
+#'  collision requires about 5^e09 keys. However, in practice this probability
+#'  depends on the chance that two runs of the RNG are accidentally initialized
+#'  with the same number, due to an improper use of \code{set.seed()} in another
+#'  function, script or package. The only way to prevent this with certainty is
+#'  to use true random keys (see \code{\link{qQQIDfactory}}). True random qQQIDs
+#'  have a 50%% collision probability in 2.7e18 keys, and this is the same at
+#'  all times, regardless of the state of the requesting machine. Thus unless
+#'  throughput of keys is a critical concern it is advisable to use true random
+#'  QQIDs over those returned by \code{rngQQID()}, or at least to initialize the
+#'  RNG with true random seed with the default \code{method = "q"}.
 #'
-#' @section qQQIds vs. rngQQIDs: In theory the chance of a key collision between
-#'   the pseudo-random QQIDs produced by \code{rngQQID()} is small if the RNG is
-#'   initialized with a 64 bit number: a 50%% probability of at least one
-#'   collision requires about 5^e09 keys. However, in practice this probability
-#'   depends on the chance that two runs of the RNG are accidentally initialized
-#'   with the same number, due to an improper use of \code{set.seed()} in
-#'   another function, script or package. The only way to prevent this with
-#'   certainty is to use true random keys (see \code{\link{qQQIDfactory}}). True
-#'   random qQQIDs have a 50%% collision probability in 2.7e18 keys, and this is
-#'   the same at all times, regardless of the state of the requesting machine.
-#'   Thus unless throughput of keys is a critical concern it is advisable to use
-#'   true random QQIDs over those returned by \code{rngQQID()}, or at least to
-#'   initialize the RNG with a seed returned from
-#'   \code{qrandom::qrandommaxint()} immediately before using \code{rngQQID()}.
+#'@section 128 vs. 122 bit random:  By default QQIDs produced with this function
+#'  can be converted to \href{https://tools.ietf.org/html/rfc4122}{RFC 4122}
+#'  compliant UUIDs. These use 6 bits to identify the method of UUID generation
+#'  and thus contain only 12 random bits. It is possible to obtain 128-bit
+#'  random QQIds from \code{rngQQID()}, by setting the parameter
+#'  \code{RFC4122compliant} to \code{FALSE}. This increases the number space
+#'  from 2^122 ≈ 5.3e+36 to 2^128 ≈ 3.4e+38 at the cost of no longer being
+#'  compliant with the UUID standard.
 #'
-#' @param n (integer) The number of pseudo-random QQIDs to return. Default is 1.
+#'@section random seeds: The function supports three methods to seed R's RNG.
+#'  The default method is \code{"q"} and will use a true random seed retrieved
+#'  from the ANU quantum random number server. An alternative is \code{"r"} and
+#'  uses R's inbuilt random initialization (cf. the behaviour of
+#'  \code{set.seed(NULL)}\link{set.seed} at \link{set.seed}. Finally, the
+#'  function can be run without a random seed with \code{"n"}, which allows
+#'  either to define one's own sane randomization, or a seed for reproducible
+#'  randomization - assuming that the risks are clearly understood. In all cases
+#'  the current state of R's RNG is saved and restored upon exit, even if the
+#'  function exits with an error. For testing purposes, saving the RNG state can
+#'  be demonstrated with mode \code{"t"} which performs exactly one call to the
+#'  RNG internally and then throws an error.
 #'
-#' @return (character) a vector of \code{n} QQIDs
+#'@section Disclaimer: Although this function has been written and tested with
+#'  care, no suitability for any particular purpose, in particular not for
+#'  high-value transactions, for applications whose failure could endager life
+#'  or property, or for cryptography is claimed. The source code is published in
+#'  full and it is up to the user to audit the code for such and similar
+#'  purposes.
 #'
-#' @author \href{https://orcid.org/0000-0002-1134-6758}{Boris Steipe} (aut)
+#'@param n (integer) The number of pseudo-random QQIDs to return. Default is 1.
+#'@param method (character) Which random seed method to use. Default is
+#'  \code{"q"}, a true random number seed, options are \code{"r"} (R's inbuilt
+#'  RNG initialization),  \code{"n"} (no initialization, if desired a
+#'  reproducible random seed can be set prior to this call, if one clearly
+#'  understands the risks), \code{"t"} (test of error handling).
+#'@param RFC4122compliant (logical) whether to base the QQID on version-stamped
+#'  128-bit numbers with 122 random bits, compliant with RFC 4122 (default), or
+#'  return QQIDs based on 128 random bits.
 #'
-#' @seealso \code{\link{qQQIDfactory}} to create a funcion that returns true
-#'   random QQIDs.
+#'@return (character) a vector of \code{n} QQIDs
+#'
+#'@author \href{https://orcid.org/0000-0002-1134-6758}{Boris Steipe} (aut)
+#'
+#'@seealso \code{\link{qQQIDfactory}} to create a function that returns true
+#'  random QQIDs.
 #'
 #' @examples
 #' \dontrun{
-#' # initialize the RNG with a random number and return 5 QQIDs
-#' set.seed(qrandom::qrandommaxint())
+#' # initialize the RNG with a true random number and return 5 QQIDs
+#' # note the latency incurred by retrieving the seed from the ANU server
 #' rngQQID(5)
 #'
-#' # return 10,000 UUIDS (takes less than two seconds)
-#' x <- qq2uu(rngQQID(1e4))
+#' # return 10,000 QQIDS and transform them into UUIDs (takes less than two
+#' # seconds); we assume that the RNG is in a sane state since we have just
+#' previously initialized it with a true random number
+#' x <- qq2uu(rngQQID(1e4, method = "n"))
 #'
 #' }
 #'
-#' @export
+#'@export
 
-rngQQID <- function(n = 1) {
+rngQQID <- function(n = 1, method = "q", RFC4122compliant = TRUE) {
   # n: number of QQids to return
+  if(is.null(n)) { return(NULL) }
+  stopifnot(is.numeric(n))
+  stopifnot(length(n) == 1)
+  stopifnot(method %in% c("q", "r", "n", "t"))
+  stopifnot(is.logical(RFC4122compliant))
+
+  testMode <- FALSE
+
+  # save the original state of the RNG
+  if ( ! exists(".Random.seed", envir = .GlobalEnv)) {
+    oSeed <- NULL
+  } else {
+    oSeed <- .Random.seed
+  }
+
+  # ensure the original RNG state will be reset when we exit
+  on.exit(if (is.null(oSeed)) {
+    rm(".Random.seed", envir=.GlobalEnv)
+  } else {
+    assign(".Random.seed", oSeed, envir=.GlobalEnv)
+  })
+
+  # set the RNG seed, dependent on the requested method
+
+  if (method == "q") { # retrieve a true random seed
+    myM <- paste("rngQQID can't retrieve a true random seed from ANU server.",
+                 "Possibly no Internet connection. Aborting." , sep = "\n")
+    rs <- tryCatch(qrandom::qrandommaxint(1),
+                   error = function(e) {
+                     message(paste("Error:", myM))
+                     NULL},
+                   warning = function(w) {
+                     message(paste("Warning:", myM))
+                     NULL})
+    if (is.null(rs)) {
+      return(invisible(NULL))  # Abort ...
+    } else {
+      set.seed(rs)
+    }
+  } else if (method == "r") { # use R's initialization
+    set.seed(NULL)
+  } else if (method == "n") { # do nothing, assume seed is already set
+    ;
+  } else if (method == "t") { # don't change the RNG, prepare to exit
+    testMode <- TRUE
+    n <- 1
+  } else {
+    stop("Unknown method - but we should never get here.")
+  }
 
   # generate matrix of n * 128 {0, 1}
-  x <- matrix(sample(0:1, 128 * n, replace = TRUE), nrow = n)
+  x <- matrix(sample(c(0, 1), 128 * n, replace = TRUE), nrow = n)
 
-  # stamp UUID version
-  x[ , 61] <- 0
-  x[ , 62] <- 1
-  x[ , 63] <- 0
-  x[ , 64] <- 0
-  x[ , 71] <- 0
-  x[ , 72] <- 1
+  if (RFC4122compliant) { # stamp UUID version
+    x[ , 61] <- 0
+    x[ , 62] <- 1
+    x[ , 63] <- 0
+    x[ , 64] <- 0
+
+    x[ , 71] <- 0
+    x[ , 72] <- 1
+  }
+
+  if (testMode) {
+    stop(bitMat2QQ(x))
+  }
 
   return(bitMat2QQ(x))
+
 }
 
 
---
+++ tests/testthat/test_rngQQID.R
@@ -0,0 +1,69 @@
+#test_rngQQID.R
+# Author: Boris Steipe <https://orcid.org/0000-0002-1134-6758>
+#
+# ==== setup ===================================================================
+#
+# Note: the tests are not entirely RNG-safe i.e. it is not guaranteed that the
+#       global state of the RNG is unchanged if testing aborts with an error.
+#       To achieve that, we would probably need to wrap this test in a function
+#       and then do the restorative assign() in an on.exit() call.
+#
+# Save current state of the RNG
+# Conveniently using code idioms by Martin Maechler on R-Help 2019-04-17
+if ( ! exists(".Random.seed", envir = .GlobalEnv)) { runif(1) }
+oSeed <- .Random.seed
+
+#
+# ==============================================================================
+context("rngQQID")
+
+set.seed(112358) # set working RNG state
+intReference <- sample.int(.Machine$integer.max, 2)
+
+set.seed(112358) # repeat working RNG state
+
+test_that("the global RNG state is as expected",  {
+  expect_equal(sample.int(.Machine$integer.max, 1), intReference[1])
+})  # we test for intReference[2] at the end of our tests
+
+test_that("corrupt input generates errors",  {
+  expect_error(rngQQID(n = "a"))
+  expect_error(rngQQID(n = 1:2))
+  expect_error(rngQQID(method = "unk"))
+  expect_error(rngQQID(RFC4122compliant = "not"))
+
+})
+
+test_that("NULL returns NULL",  {
+  expect_true(is.null(rngQQID(NULL)))
+})
+
+
+# There is no reasonable test for the true random seed
+test_that("valid numeric input produces the expected output",  {
+  expect_equal(rngQQID(n = 1, method = "n"),
+               "wolf.pail.jW_nrfROUrepp4nfsR")
+  expect_equal(rngQQID(n = 1, method = "n"),   # same result again since we are
+               "wolf.pail.jW_nrfROUrepp4nfsR") # resetting the RNG
+  expect_equal(rngQQID(n = 1, method = "n", RFC4122compliant = FALSE),
+               "wolf.pail.jW_nrfR-Qrepp4nfsR") # similar but different
+               #                 ^^
+})
+
+test_that("the RNG state is properly restored on error",  {
+  expect_error(rngQQID(n = 1, method = "t"), "wolf.pail.jW_nrfROUrepp4nfsR")
+  expect_equal(rngQQID(n = 1, method = "n"),   # same result - the RNG state
+               "wolf.pail.jW_nrfROUrepp4nfsR") # should be unchanged
+})
+
+test_that("the global RNG state has not been changed by our tests",  {
+  expect_equal(sample.int(.Machine$integer.max, 1), intReference[2])
+})
+
+
+# ==== teardown ================================================================
+#
+# Restore original state of the RNG
+assign(".Random.seed", oSeed, envir=.GlobalEnv)
+
+# [END]
