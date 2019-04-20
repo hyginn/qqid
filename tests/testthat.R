d3ef5ce6e30f84050b79cab8657ab1ca2ef755ab
hyginn <boris.steipe@utoronto.ca>
2019-04-19 21:22:44 -0400

    remove dynamic access of packagename - not a good idea in the first place and and caused problems with covr

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,7 @@
+# load dependencies
 library(testthat)
+library(qqid)
 
-# parse the package name from the parent directory path (necessary because
-# normal tests and R CMD Check tests are run from different directories)
-x <- unlist(strsplit(gsub("(\\.Rcheck/tests)?$", "", getwd()), "/"))
-pkgName <- x[length(x)]
+test_check("qqid")
 
-library(pkgName, character.only = TRUE)
-
-test_check(pkgName)
 # [END]
