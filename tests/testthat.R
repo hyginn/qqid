c324b39bd2fa4e572b71bf29ea71eec1a253c591
Siegfried Köstlmeier <siegfried.koestlmeier@gmail.com>
2020-01-13 10:07:56 +0100

    Update testthat.R
    The package is based on the Quantum Random Number Generator provided by the Australian National University. Tests should only run if their corresponding API is available, i.e. their website (https://qrng.anu.edu.au/index.php) is accessible which is check by the HTTP status code 200.

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,22 @@
 library(testthat)
 library(qqid)
 
-test_check("qqid")
+# check if the ANU Quantum Random Number Generator API is available 
+check_qrng <- function(){
+  tryCatch(
+    expr = {
+      req <- curl::curl_fetch_memory('https://qrng.anu.edu.au/index.php')
+      req$status_code
+    },
+    error = function(e){
+      -1
+    }
+  )
+}
+
+## run tests only if internet access and the QRNG API is both available
+if(curl::has_internet() & check_qrng() == 200){
+  test_check("qqid")
+}
 
 # [END]
